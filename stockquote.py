eb03ebef96fc58eba40387e6aa6280c302133a7a
Richard Chen <richardchen39@gmail.com>
2015-11-10 15:42:26 -0800

    extracts data from quote

---
+++ stockquote.py
@@ -0,0 +1,35 @@
+from yahoo_finance import Share
+from parser import parse
+import sys
+
+def get_quote(ticker):
+	return Share(ticker).get_price()
+
+def get_pe_ratio(ticker):
+	return Share(ticker).get_price_earnings_ratio()
+
+def get_ebitda(ticker):
+	ebitda = Share(ticker).get_ebitda()
+	if ebitda[-1] == 'M':
+		ebitda = float(ebitda[:-1]) * 10**6
+	elif ebitda[-1] == 'B':
+		ebitda = float(ebitda[:-1]) * 10**9
+	return ebitda
+
+def get_fcf(ticker):
+	try:
+		num = parse(ticker)
+		current_assets = num.next()
+		ppe = num.next()
+		current_liabilities = num.next()
+	except StopIteration:
+		return 'missing fcf'
+
+	# print current_assets, current_liabilities, ppe
+	return get_ebitda(ticker) - (current_assets - current_liabilities) - ppe 
+
+ticker = raw_input('Enter ticker symbol: ').lower()
+print get_quote(ticker)
+print get_pe_ratio(ticker)
+print get_ebitda(ticker)
+print get_fcf(ticker)
